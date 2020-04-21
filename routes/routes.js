3e369ed0fe71c8ca93578036d6348145310e7676
rdfsouza <rodolfovsouza@gmail.com>
2020-04-21 20:25:10 +0000

    post

--- routes/routes.js
+++ routes/routes.js
@@ -2,40 +2,40 @@ const express = require("express");
 const Route = express();
 const Tickets = require('../data');
 
+
 require('../DB');
 
+
 Route.get('/tickets', (req, res) => {
-Tickets.find({}).then((tickets)  => {
-    res.send(tickets);
+    Tickets.find({}).then((tickets) => {
+        res.send(tickets);
+    });
 });
-});
+
 
 Route.post('/tickets', (req, res) => {
     Tickets.create(req.body).then((tickets) => {
-         res.send(tickets);
-        });
-    });
-   
-
-    Route.get('/tickets/', (req, res) => {
-     
-    });
-
-    Route.delete('/tickets/:id', (req, res) => {
-        Tickets.findByIdAndRemove({_id: req.params.id})
-        .then((tickets) => {
         res.send(tickets);
+    });
+});
+
+
+Route.delete('/tickets/:id', (req, res) => {
+    Tickets.findByIdAndRemove({ _id: req.params.id })
+        .then((tickets) => {
+            res.send(tickets);
+        });
+});
+
+Route.put('/tickets/:id', (req, res) => {
+    Tickets.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+        Tickets.findOne({ _id: req.params.id }).then((tickets) => {
+            res.send(tickets);
         });
     });
-
-    Route.put('/tickets/:id', (req, res) => {
-           Tickets.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
-               Tickets.findOne({_id: req.params.id}).then((tickets) => {
-   res.send(tickets);
-               });
-        });
-    });
+});
 
 
 
-    module.exports = Route;
+module.exports = Route;
+
